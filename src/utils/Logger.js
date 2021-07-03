fc88f82914309c1d9a51e6c4e89bbaaec8020c56
armr-dev <andrermr.dev@gmail.com>
2021-07-02 23:08:22 -0400

    🚨 fix linter errors in Logger.js

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,12 +1,11 @@
+/* eslint-disable no-console */
 /**
    * A Logger to log the events throughout the application.
-   * 
+   *
    * The possible levels are:
    *  0 - ERROR | 1 - INFO | 2 - DEBUG
    */
 class Logger {
-  level;
-
   /**
    * @param {number} level - The level of the Logger
    * 0 - ERROR | 1 - INFO | 2 - DEBUG
@@ -19,26 +18,26 @@ class Logger {
     if (this.level >= 0) {
       console.log(`ERROR: ${message}`);
     }
-  };
+  }
 
   info(message) {
     if (this.level >= 1) {
       console.log(`INFO: ${message}`);
     }
-  };
+  }
 
   debug(message) {
     if (this.level >= 2) {
       console.log(`DEBUG: ${message}`);
     }
-  };
+  }
 }
 
 let logger;
 
 export function initLogger(level) {
   let loggerLevel = 0;
-  
+
   if (level) { loggerLevel = level; }
 
   logger = new Logger(loggerLevel);
@@ -52,8 +51,6 @@ function getLogger() {
   return logger;
 }
 
-export function logError(message) { getLogger().error(message) };
-export function logInfo(message) { getLogger().info(message) };
-export function logDebug(message) { getLogger().debug(message) };
-
-export default logger;
+export function logError(message) { getLogger().error(message); }
+export function logInfo(message) { getLogger().info(message); }
+export function logDebug(message) { getLogger().debug(message); }
